371503986e857a60d2a1779556514281021faa1b
Arnaud301 <arnaud2bonnet@gmail.com>
2024-12-10 14:12:27 +0100

    did the 9th task with the help ludiane

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,19 +7,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int i = 0;
-	int j = 0;
+	size_t count_left = 0, count_right = 0;
 
 	if (!tree)
 		return (0);
-	if (tree->left != NULL)
-	{
-		i++;
-		binary_tree_height(tree = tree->left);
-	}
-	if (tree->right != NULL)
-	{
-		j++;
-		binary_tree_height(tree = tree->right);
-	}
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	count_left = binary_tree_height(tree->left);
+	count_right = binary_tree_height(tree->right);
+
+	if (count_left >= count_right)
+		return (count_left + 1);
+	else
+		return (count_right + 1);
 }
