0fa2fee63c67be9289e0c139b6b8ae6c52b45a92
GabrielCoh <9924@holbertonstudents.com>
2024-12-10 16:11:28 +0000

    started the fourteenth but we will have to finish it

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - function that returns the height of a tree
+ * @tree: the current tree were gonna get the height from.
+ * Return: 0 if theres no tree else we return the height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t count_left = 0, count_right = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	count_left = binary_tree_height(tree->left);
+	count_right = binary_tree_height(tree->right);
+
+	if (count_left >= count_right)
+		return (count_left + 1);
+	else
+		return (count_right + 1);
+}
+
+/**
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t count_left = 0, count_right = 0;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->left || !tree->right)
+}
