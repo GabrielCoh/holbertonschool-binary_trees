f40a786ef339267a28ec7d77a998ee04055985f6
Arnaud301 <arnaud2bonnet@gmail.com>
2024-12-10 15:16:37 +0100

    forgot the bettydoc again...

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,10 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
- *
+ * binary_tree_size - function that calculates the size of a tree.
+ * @tree: the current tree to use the function on.
+ * Return: 0 if theres no tree else return the size of the tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
