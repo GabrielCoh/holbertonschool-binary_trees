82153dc0e1c9a17e10f89a0d88bce1f988d5230a
Arnaud301 <arnaud2bonnet@gmail.com>
2024-12-10 10:17:16 +0100

    1th task done in main

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ *
+ *
+ *
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *left_n;
+
+	if (!parent)
+		return (NULL);
+
+	left_n = binary_tree_node(NULL, value);
+
+	if (!left_n)
+		return (NULL);
+
+	if (parent->left)
+	{
+		parent->left->parent = left_n;
+		left_n->left = parent->left;
+	}
+
+	parent->left = left_n;
+	left_n->parent = parent;
+
+	return (left_n);
+}
